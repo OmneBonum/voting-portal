2c9f61c2dc56303af349a7d049ba189c768599bd
Hritik-code <hritik@codenomad.net>
2022-02-03 10:58:47 +0530

    again

--- vote/models.py
+++ vote/models.py
@@ -5,8 +5,6 @@ from django.utils.translation import gettext_lazy as _
 from .manager import CustomUserManager
 from django.core.validators import validate_comma_separated_integer_list
 
-
-
 # Create your models here.
 class user(AbstractBaseUser,PermissionsMixin):
     district=models.CharField(max_length=200)
@@ -59,3 +57,111 @@ class pod_groups_members(models.Model):
 #   Elect_count=models.IntegerField(default=1)
 #   Elect_user=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
 #   Elect_member=models.ForeignKey(pod_member,on_delete=models.CASCADE,null=True)
+
+class Firstdel_groups(models.Model):
+  group_key =  models.CharField(unique=True, editable=False,max_length=6)
+  group_owner=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class firstdel_groups_members(models.Model):
+  group=models.ForeignKey(pod_groups,on_delete=models.CASCADE,null=True)
+  member=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  vote_count=models.IntegerField(default=0)
+  elect_count=models.IntegerField(default=0)
+  member_status = models.IntegerField(null=True) 
+  member_comment = models.TextField(max_length=500,null=True)
+  vote_given=models.IntegerField(default=0)
+  elect_vote_given=models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)  
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class seconddel_groups(models.Model):
+  group_key =  models.CharField(unique=True, editable=False,max_length=6)
+  group_owner=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class seconddel_groups_members(models.Model):
+  group=models.ForeignKey(pod_groups,on_delete=models.CASCADE,null=True)
+  member=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  vote_count=models.IntegerField(default=0)
+  elect_count=models.IntegerField(default=0)
+  member_status = models.IntegerField(null=True) 
+  member_comment = models.TextField(max_length=500,null=True)
+  vote_given=models.IntegerField(default=0)
+  elect_vote_given=models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)  
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class thirddel_groups(models.Model):
+  group_key =  models.CharField(unique=True, editable=False,max_length=6)
+  group_owner=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class thirddel_groups_members(models.Model):
+  group=models.ForeignKey(pod_groups,on_delete=models.CASCADE,null=True)
+  member=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  vote_count=models.IntegerField(default=0)
+  elect_count=models.IntegerField(default=0)
+  member_status = models.IntegerField(null=True) 
+  member_comment = models.TextField(max_length=500,null=True)
+  vote_given=models.IntegerField(default=0)
+  elect_vote_given=models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)  
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class fourthdel_groups(models.Model):
+  group_key =  models.CharField(unique=True, editable=False,max_length=6)
+  group_owner=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class fourthdel_groups_members(models.Model):
+  group=models.ForeignKey(pod_groups,on_delete=models.CASCADE,null=True)
+  member=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  vote_count=models.IntegerField(default=0)
+  elect_count=models.IntegerField(default=0)
+  member_status = models.IntegerField(null=True) 
+  member_comment = models.TextField(max_length=500,null=True)
+  vote_given=models.IntegerField(default=0)
+  elect_vote_given=models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)  
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class fifthdel_groups(models.Model):
+  group_key =  models.CharField(unique=True, editable=False,max_length=6)
+  group_owner=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class fifthdel_groups_members(models.Model):
+  group=models.ForeignKey(pod_groups,on_delete=models.CASCADE,null=True)
+  member=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  vote_count=models.IntegerField(default=0)
+  elect_count=models.IntegerField(default=0)
+  member_status = models.IntegerField(null=True) 
+  member_comment = models.TextField(max_length=500,null=True)
+  vote_given=models.IntegerField(default=0)
+  elect_vote_given=models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)  
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class sixthdel_groups(models.Model):
+  group_key =  models.CharField(unique=True, editable=False,max_length=6)
+  group_owner=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)
+  updated_at =  models.DateTimeField(auto_now=True)
+
+class sixthdel_groups_members(models.Model):
+  group=models.ForeignKey(pod_groups,on_delete=models.CASCADE,null=True)
+  member=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+  vote_count=models.IntegerField(default=0)
+  elect_count=models.IntegerField(default=0)
+  member_status = models.IntegerField(null=True) 
+  member_comment = models.TextField(max_length=500,null=True)
+  vote_given=models.IntegerField(default=0)
+  elect_vote_given=models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True,null=True)  
+  updated_at =  models.DateTimeField(auto_now=True)
