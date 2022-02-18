f13c5b974c8434342a39541c2aaed95691b595b3
webdeveloper510 <developerweb6@gmail.com>
2022-02-18 19:09:28 +0530

    again

--- Voting_portal/settings.py
+++ Voting_portal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.version_info = (1, 4, 13, "final", 0)
+
+pymysql.install_as_MySQLdb()
 
 
 
