44059a2a0a131fc40a9438d5c57c75027a9e0849
webdeveloper510 <developerweb6@gmail.com>
2022-02-19 15:07:59 +0530

    header

--- vote/Views/Loginview.py
+++ vote/Views/Loginview.py
@@ -17,9 +17,39 @@ from django.urls import reverse
 
 
 def index(request):
-    context = {}
-    template = loader.get_template('app/index.html')
-    return HttpResponse(template.render(context, request))
+    con=pod_groups.objects.all()
+    fcon=seconddel_groups.objects.all()
+    context = sixthdel_groups.objects.all()
+    s_key=seconddel_groups.objects.filter(group_owner_id=request.user.id)
+    skey=s_key.values_list("group_key",flat=True).first()
+    print("pod",skey)
+    f_key=pod_groups.objects.filter(group_owner_id=request.user.id)
+    pkey=f_key.values_list("group_key",flat=True).first()
+    print("pod",pkey)
+    pod_key=sixthdel_groups.objects.filter(group_owner_id=request.user.id)
+    kkey=pod_key.values_list("group_key",flat=True).first()
+    print("pod",kkey)
+    if pod_groups.objects.filter(group_key=pkey):
+      if firstdel_groups.objects.filter(group_owner_id=request.user.id):
+        if seconddel_groups.objects.filter(group_owner_id=request.user.id):
+          if thirddel_groups.objects.filter(group_owner_id=request.user.id): 
+            if fourthdel_groups.objects.filter(group_owner_id=request.user.id): 
+              if fifthdel_groups.objects.filter(group_owner_id=request.user.id):
+                if sixthdel_groups.objects.filter(group_owner_id=request.user.id):
+                  return render(request,"app/index.html",{'context':context,'kkey':kkey,'fcon':fcon,'pkey':skey,'z':0})  
+                else:
+                  return render(request,"app/index.html",{'context':context,'kkey':kkey,'fcon':fcon,'pkey':skey,'si':0})
+              else:
+                return render(request,"app/index.html",{'context':context,'kkey':kkey,'fcon':fcon,'pkey':skey,'fi':0})
+            else:
+              return render(request,"app/index.html",{'context':context,'kkey':kkey,'fcon':fcon,'pkey':skey,'fo':0})
+          else:
+            return render(request,"app/index.html",{'context':context,'kkey':kkey,'fcon':fcon,'pkey':skey,'th':0 }) 
+        else:
+          return render(request,"app/index.html",{'context':context,'kkey':kkey,'con':con,'pkey':pkey,'s':0}) 
+      else:  
+        return render(request,"app/index.html",{'context':context,'kkey':kkey,'con':con,'pkey':pkey,'f':0})  
+    return render(request,"app/index.html",{'context':context,'kkey':kkey,'con':con,'pkey':pkey,'f': 0}) 
 
 def user_login(request):
     if request.method == "POST":
@@ -49,18 +79,25 @@ def user_login(request):
             pkey=pod_key.values_list("group_key",flat=True).first()
             print("pod",pkey)
             
+            pod_key=sixthdel_groups.objects.filter(group_owner_id=request.user.id)
+            kkey=pod_key.values_list("group_key",flat=True).first()
+            print("pod",kkey)
+            
             #group_keys=pkey[0]
             #print(group_keys[0])
             
-            if firstdel_groups.objects.filter(group_owner_id=request.user.id) and firstdel_groups_members.objects.filter(member_id=request.user.id):
+            if firstdel_groups.objects.filter(group_owner_id=request.user.id) or firstdel_groups_members.objects.filter(member_id=request.user.id):
                 
 
-                if seconddel_groups.objects.filter(group_owner_id= request.user.id) and seconddel_groups_members.objects.filter(member_id=request.user.id):
-                    if thirddel_groups.objects.filter(group_owner_id= request.user.id) and thirddel_groups_members.objects.filter(member_id=request.user.id):
-                      if fourthdel_groups.objects.filter(group_owner_id= request.user.id) and fourthdel_groups_members.objects.filter(member_id=request.user.id):  
+                if seconddel_groups.objects.filter(group_owner_id= request.user.id) or seconddel_groups_members.objects.filter(member_id=request.user.id)  :
+                    if thirddel_groups.objects.filter(group_owner_id= request.user.id) or thirddel_groups_members.objects.filter(member_id=request.user.id):
+                      if fourthdel_groups.objects.filter(group_owner_id= request.user.id) or fourthdel_groups_members.objects.filter(member_id=request.user.id):  
                         
                         if fifthdel_groups.objects.filter(group_key= pkey) and fifthdel_groups_members.objects.filter(member_id=request.user.id):
-                          return redirect('/fishow') 
+                          if sixthdel_groups.objects.filter(group_owner_id= request.user.id) and sixthdel_groups_members.objects.filter(member_id=request.user.id):
+                            return redirect('/sishow') 
+                          else:
+                            return redirect('/fishow') 
                         else:
                           return redirect('/foshow') 
 
