b4882d4c5202e524010ca9ec5f794221b6152628
webdeveloper510 <developerweb6@gmail.com>
2022-02-23 17:53:26 +0530

    again

--- vote/Views/firstchatview.py
+++ vote/Views/firstchatview.py
@@ -154,6 +154,6 @@ def fgetMessages(request):
             z=i.group_id
     
 
-    messages = firstMessage.objects.filter(room=z)
+    messages = firstMessage.objects.filter(room=z)#ll
     print("messages",messages)
     return JsonResponse({"messages":list(messages.values())})
